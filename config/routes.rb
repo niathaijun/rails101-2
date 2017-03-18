8536e55b702bd0882241430e4f55002259136e97
langweibo <niathaijun2016@gmail.com>
2017-03-18 20:53:58 +0800

    implement post create function

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
     resources :posts
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :groups
+
   root 'groups#index'
 end
